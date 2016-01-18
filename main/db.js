4b2d86d4c7721bdc50df66f269866b80e71d29b4
akto1993 <akto1993@gmail.com>
2016-01-18 17:29:03 +0100

    pierwsze zmiany

--- main/db.js
+++ main/db.js
@@ -1,24 +1,24 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/leki', function(err) {
+mongoose.connect('mongodb://localhost/warzywa', function(err) {
   if(err) {
     console.log(err);
   } else {
     console.log('Connected to the database');
   }
 });
-var leki = mongoose.model('lekis', new mongoose.Schema({
+var warzywa = mongoose.model('warzywas', new mongoose.Schema({
     _id : String,
     name: String,
     type: String,
     cost: String,
 }));
 
-var lek_add = function() {
-  new leki({
+var warzywo_add = function() {
+  new warzywa({
     _id:          mongoose.Types.ObjectId(),
-    name:         $("#lek_name").val(),
-    type:         $("#lek_type").val(),
-    cost:         $("#lek_cost").val(),
+    name:         $("#warzywo_name").val(),
+    type:         $("#warzywo_type").val(),
+    cost:         $("#warzywo_cost").val(),
   }).save(function(err) {
     if(err) {
       console.log(err);
@@ -31,19 +31,19 @@ var lek_add = function() {
 };
 
 var wypisz = function(){
-  leki.find({}, function(err, result) {
+  warzywa.find({}, function(err, result) {
     if(err) {
       console.log(err);
     } else {
       $('.sekcja').remove();
       var content = "";
-      $(".leki").append("<table class='sekcja'><tr><td>Nazwa</td><td>Typ</td><td>Cena</td><td>Usun</td><td>Edytuj</td></tr></table>");
+      $(".warzywa").append("<table class='sekcja'><tr><td>Nazwa</td><td>Typ</td><td>Cena</td><td>Usun</td><td>Edytuj</td></tr></table>");
       for(var i = 0; i < result.length; i++) {
         content +=    "<tr><td>"+result[i].name+"</td>"+
                       "<td>"+result[i].type+"</td>"+
                       "<td>"+result[i].cost+"</td>"+
-                      "<td><input type='submit' value='Delete' onclick='lek_delete("+JSON.stringify(result[i]._id)+")'></td>"+
-                      "<td><input type='submit' value='Edytuj' onclick='lek_edycja("+JSON.stringify(result[i]._id)+")'></td></tr>";
+                      "<td><input type='submit' value='Delete' onclick='warzywo_delete("+JSON.stringify(result[i]._id)+")'></td>"+
+                      "<td><input type='submit' value='Edytuj' onclick='warzywo_edycja("+JSON.stringify(result[i]._id)+")'></td></tr>";
       }
       $(".sekcja").append(content);
     }
@@ -53,8 +53,8 @@ $().ready(function() {
   wypisz();
 });
 
-var lek_delete = function(id) {
-  leki.find({ _id: id }, function(err, result) {
+var warzywo_delete = function(id) {
+  warzywa.find({ _id: id }, function(err, result) {
     if(err) {
       console.log('problem z pobraniem');
     }
@@ -69,9 +69,9 @@ var lek_delete = function(id) {
 wypisz();
 };
 
-var lek_edycja = function(id) {
+var warzywo_edycja = function(id) {
   $(".edit").show();
-  leki.findOne({ _id: id }, function(err, result) {
+  warzywa.findOne({ _id: id }, function(err, result) {
     if(err) {
       console.log(err); next();
     } else {
@@ -83,8 +83,8 @@ var lek_edycja = function(id) {
   });
 };
 
-var lek_edytujdb = function(id) {
-  leki.find({ _id: $("#edit_id").val() }, function(err, result) {
+var warzywo_edytujdb = function(id) {
+  warzywa.find({ _id: $("#edit_id").val() }, function(err, result) {
     if(err) {
       console.log(err);
     } else {
